ba63d4307107cfab0133ed7646e9e02fb2913716
liam <clink.6@osu.edu>
2024-12-27 16:55:17 -0500

    Made characteristic curve for rotsym potential have multiple slopes

--- electron-ion optics/characteristic_curves.py
+++ electron-ion optics/characteristic_curves.py
@@ -1,56 +1,83 @@
+""" 
+Gives the curves of paraxial electrons given a rotationally symmetric potential
+constrained on the optical axis
+"""
+
 import numpy as np
 from scipy import integrate
 import matplotlib.pyplot as plt
 
 
-def phi(z):
+def phi(z: float):
+    """Potential along the optical axis
+
+    Args:
+        z (float): Distance along the optical axis
+
+    Returns:
+        float: electric potential at z
+    """
     return 1.0 + 0.5 * np.sin(z)
 
 
-def phi_prime(z):
+def phi_prime(z: float):
+    """z derivative of the potential on the axis
+
+    Args:
+        z (float): Distance along the optical axis
+
+    Returns:
+        float: Derivative of electric potential at z
+    """
     return 0.5 * np.cos(z)
 
 
-def rot_symm_field(z, X):
-    return [X[1], -3.0 / 16.0 * (phi_prime(z) / phi(z)) ** 2 * X[0]]
+def rot_symm_field(z: float, position_slope_vector: list) -> list:
+    """
+    ODE to be fed into scipy.solve_ivp
+
+    Args:
+        z (float): Axial distance
+        position_slope_vector (List with two floats):
+            First element is transverse distance,
+            Second element is the z derivative of the transverse distance
+
+    Returns:
+        list: List of derivatives of the input. That is,
+            the first element is the first derivative of the transverse distance,
+            and the second element is the second derivative of the transverse distance
+    """
+    return [
+        position_slope_vector[1],
+        -3.0 / 16.0 * (phi_prime(z) / phi(z)) ** 2 * position_slope_vector[0],
+    ]
 
 
-def integrand(t, Y):
-    return [Y[1], -2 * Y[0] - Y[1]]
-
-
-def main():
-    slopes = np.arange(-0.1, 0.1, 10)
-    times = np.arange(0, 25.0, 0.001)
+if __name__ == "__main__":
+    # Calculate and plot the trajectories
+    slopes = np.linspace(-0.2, 0.2, 10)
+    z_vals = np.arange(0, 25.0, 0.001)
     tracks = []
-    # for slope in slopes:
-    solution = integrate.solve_ivp(
-        rot_symm_field,
-        t_span=[times[0], times[-1]],
-        y0=[
-            1.0,
-            0.0,
-        ],  # Initial values should be computed with the potential, because the initial values are of R = r*phi**.25, not r itself
-        t_eval=times,
-    )
-    plt.plot(
-        solution.t,
-        solution.y[0, :] / phi(solution.t) ** 0.25,
-        label="y",
-    )
-    plt.plot(solution.t, phi(solution.t), label="potential at y=0")
-    plt.plot(
-        solution.t,
-        phi(solution.t) + solution.y[0, :] ** 2 / 4 * np.sin(solution.t),
-        label="potential at position",
-    )
+    # Initial values (y0) should be computed with the potential,
+    # because the initial values are of R = r*phi**.25, not r itself
+    for slope in slopes:
+        print(slope)
+        solution = integrate.solve_ivp(
+            rot_symm_field,
+            t_span=[z_vals[0], z_vals[-1]],
+            y0=[
+                1.0,
+                slope,
+            ],
+            t_eval=z_vals,
+        )
+        plt.plot(
+            solution.t,
+            solution.y[0, :] / phi(solution.t) ** 0.25,
+        )
+
     plt.xlabel("axial coordinate")
     plt.ylabel("transverse coordinate")
     plt.title("Electron Trajectory in oscillating rotationally symmetric potential")
     plt.gca().set_aspect("equal")
-    plt.legend()
     plt.show()
-
-
-if __name__ == "__main__":
-    main()
